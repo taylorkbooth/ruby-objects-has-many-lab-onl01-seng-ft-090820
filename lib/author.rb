18147a817f8ca037028391a1c8275ab0be3d440e
Taylor Booth <taylorkbooth@gmail.com>
2020-09-24 16:41:17 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -7,14 +7,27 @@ class Author
     @name = name
   end
   
+  def songs
+    Post.all.select do |song|
+      song.artist == self
+      
+    end
+  end
   
+  def add_song(song)
+    song.artist = self
+  end
   
-  
-  
-  
-  
-  
-  
+ def add_song_by_name(song_name)
+   singer = Post.new(song_name)
+   add_song(singer)
+ end
+ 
+ def self.song_count
+   Post.all.count
+end
+
+   
   
   
 end
