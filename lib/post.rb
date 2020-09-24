4a06db91f36d30fcbaa23ebac967b22205b903a9
Taylor Booth <taylorkbooth@gmail.com>
2020-09-24 16:38:52 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -2,13 +2,28 @@ class Post
   
     attr_accessor :title
   
+  @@all= []
   
   def initialize(title)
     @title = title
+    @@all << self
   end
   
   
+   def self.all
+    @@all
+  end
   
+ def artist_name
+    if self.artist
+      self.artist.name
+    else
+      nil
+    end
+  end
+  
+  
+end
   
   
   
